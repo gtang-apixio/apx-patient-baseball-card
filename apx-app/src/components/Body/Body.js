a6c9335bf5822bae74433157efb1a5f529587013
Greg Tang <gtang@APX2184.local>
2020-02-27 14:40:42 -0800

    Created deleteRange function for filter

--- apx-app/src/components/Body/Body.js
+++ apx-app/src/components/Body/Body.js
@@ -90,7 +90,6 @@ class Body extends React.Component {
         bmiRange.push(20);
       } else if (name === "bmi20_25") {
         bmiRange.push(20, 25);
-        this.setState({ bmiRange: bmiRange });
       } else if (name === "bmi25_30") {
         bmiRange.push(25, 30);
       } else if (name === "bmi30_35") {
@@ -105,12 +104,11 @@ class Body extends React.Component {
     if (name.substring(0, 3) === "a1c") {
       if (name === "a1cUnder4") {
         a1cRange.push(4);
-      } else if (name === "bmi4_7") {
+      } else if (name === "a1c4_7") {
         a1cRange.push(4, 7);
-        this.setState({ bmiRange: bmiRange });
-      } else if (name === "bmi7_10") {
+      } else if (name === "a1c7_10") {
         a1cRange.push(7, 10);
-      } else if (name === "bmiAbove10") {
+      } else if (name === "a1cAbove10") {
         a1cRange.push(10);
       }
       this.setState({ a1cRange: a1cRange });
@@ -132,34 +130,120 @@ class Body extends React.Component {
 
     // gender
     if (name === "male") {
-      dbpRange.push("male");
+      genderOption.push("male");
       this.setState({ genderOptions: genderOption });
     } else if (name === "female") {
-      dbpRange.push("female");
+      genderOption.push("female");
       this.setState({ genderOptions: genderOption });
     }
 
     // age
     if (name.substring(0, 3) === "age") {
       if (name === "ageUnder55") {
-        dbpRange.push(55);
+        ageRange.push(55);
       } else if (name === "age55_60") {
-        dbpRange.push(55, 60);
+        ageRange.push(55, 60);
       } else if (name === "age60_65") {
-        dbpRange.push(60, 65);
+        ageRange.push(60, 65);
       } else if (name === "age65_70") {
-        dbpRange.push(65, 70);
+        ageRange.push(65, 70);
       } else if (name === "age70_75") {
-        dbpRange.push(70, 75);
+        ageRange.push(70, 75);
       } else if (name === "ageAbove75") {
-        dbpRange.push(75);
+        ageRange.push(75);
       }
       this.setState({ ageRange: ageRange });
     }
   };
 
   deleteRange = name => {
-    console.log("Delete range");
+    let bmiRange = [...this.state.bmiRange];
+    let a1cRange = [...this.state.a1cRange];
+    let dbpRange = [...this.state.dbpRange];
+    let genderOption = [...this.state.genderOptions];
+    let ageRange = [...this.state.ageRange];
+
+    // bmi
+    if (name.substring(0, 3) === "bmi") {
+      if (name === "bmiUnder20") {
+        bmiRange.splice(bmiRange.indexOf(20), 1);
+      } else if (name === "bmi20_25") {
+        bmiRange.splice(bmiRange.indexOf(20), 1);
+        bmiRange.splice(bmiRange.indexOf(25), 1);
+      } else if (name === "bmi25_30") {
+        bmiRange.splice(bmiRange.indexOf(25), 1);
+        bmiRange.splice(bmiRange.indexOf(30), 1);
+      } else if (name === "bmi30_35") {
+        bmiRange.splice(bmiRange.indexOf(30), 1);
+        bmiRange.splice(bmiRange.indexOf(35), 1);
+      } else if (name === "bmiAbove35") {
+        bmiRange.splice(bmiRange.indexOf(35), 1);
+      }
+      this.setState({ bmiRange: bmiRange });
+    }
+
+    // a1c
+    if (name.substring(0, 3) === "a1c") {
+      if (name === "a1cUnder4") {
+        a1cRange.splice(a1cRange.indexOf(4), 1);
+      } else if (name === "a1c4_7") {
+        a1cRange.splice(a1cRange.indexOf(4), 1);
+        a1cRange.splice(a1cRange.indexOf(7), 1);
+      } else if (name === "a1c7_10") {
+        a1cRange.splice(a1cRange.indexOf(7), 1);
+        a1cRange.splice(a1cRange.indexOf(10), 1);
+      } else if (name === "a1cAbove10") {
+        a1cRange.splice(a1cRange.indexOf(10), 1);
+      }
+      this.setState({ a1cRange: a1cRange });
+    }
+
+    // dbp
+    if (name.substring(0, 3) === "dbp") {
+      if (name === "dbpUnder80") {
+        dbpRange.splice(dbpRange.indexOf(80), 1);
+      } else if (name === "dbp80_100") {
+        dbpRange.splice(dbpRange.indexOf(80), 1);
+        dbpRange.splice(dbpRange.indexOf(100), 1);
+      } else if (name === "dbp100_120") {
+        dbpRange.splice(dbpRange.indexOf(100), 1);
+        dbpRange.splice(dbpRange.indexOf(120), 1);
+      } else if (name === "dbpAbove120") {
+        dbpRange.splice(dbpRange.indexOf(120), 1);
+      }
+      this.setState({ dbpRange: dbpRange });
+    }
+
+    // gender
+    if (name === "male") {
+      genderOption.splice(genderOption.indexOf("male"), 1);
+      this.setState({ genderOptions: genderOption });
+    } else if (name === "female") {
+      genderOption.splice(genderOption.indexOf("female"), 1);
+      this.setState({ genderOptions: genderOption });
+    }
+
+    // age
+    if (name.substring(0, 3) === "age") {
+      if (name === "ageUnder55") {
+        ageRange.splice(ageRange.indexOf(55), 1);
+      } else if (name === "age55_60") {
+        ageRange.splice(ageRange.indexOf(55), 1);
+        ageRange.splice(ageRange.indexOf(60), 1);
+      } else if (name === "age60_65") {
+        ageRange.splice(ageRange.indexOf(60), 1);
+        ageRange.splice(ageRange.indexOf(65), 1);
+      } else if (name === "age65_70") {
+        ageRange.splice(ageRange.indexOf(65), 1);
+        ageRange.splice(ageRange.indexOf(70), 1);
+      } else if (name === "age70_75") {
+        ageRange.splice(ageRange.indexOf(70), 1);
+        ageRange.splice(ageRange.indexOf(75), 1);
+      } else if (name === "ageAbove75") {
+        ageRange.splice(ageRange.indexOf(75), 1);
+      }
+      this.setState({ ageRange: ageRange });
+    }
   };
 
   handleRange = name => {
